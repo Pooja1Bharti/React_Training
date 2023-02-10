141ac576e84d5db7094c633baa00ee7840d3e4cb
Pooja1Bharti <124995519+Pooja1Bharti@users.noreply.github.com>
2023-02-10 14:07:53 +0530

    Add files via upload

---
+++ Assignment_5/script.js
@@ -0,0 +1,7 @@
+//document.write("hello world")
+function validateform(){
+  var eamil=  document.getElementById("email")
+  var password = document.getElementById("password")
+  if(eamil.value != "" || password.value != "")
+  alert("logged in successfully!!!!!")
+}
