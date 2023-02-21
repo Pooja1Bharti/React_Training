73a819dc98dccb29ecdecdedb5c6107381020f79
Pooja1Bharti <124995519+Pooja1Bharti@users.noreply.github.com>
2023-02-21 12:13:20 +0530

    Add files via upload

---
+++ assignment-10/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+
+
+import LifeCycle, { Demo } from './class-component/LifeCycle';
+
+function App() {
+  return (
+    <div className="App">
+    <Demo />
+      <LifeCycle />
+    </div>
+  );
+}
+
+export default App;
