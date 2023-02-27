c128eabe1761cc3c2ecce35198bed31a0b5bdaee
Pooja1Bharti <124995519+Pooja1Bharti@users.noreply.github.com>
2023-02-27 15:41:52 +0530

    Add files via upload

---
+++ assesment_ecommerce/App.js
@@ -0,0 +1,19 @@
+
+import './App.css';
+import ProductList from './component/ProductList';
+
+import Products from './component/Products.js';
+
+
+
+function App() {
+  return (
+    <div className="App">
+    <h3>Products-App</h3>
+    <ProductList />
+    <Products />
+  </div>
+  );
+}
+
+export default App;
